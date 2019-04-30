fb7e6da5a8c5ab84b67870a82ec69ad4406f9097
PAPAIAHGARI SANJAY <50132428+SanjayPapaiahgari@users.noreply.github.com>
2019-04-30 15:35:55 +0530

    Update Bubblesort.py

--- Bubblesort.py
+++ Bubblesort.py
@@ -1,15 +1,8 @@
 # Sorting a list of numbers using Bubble Sort in Python
-a=[]
-for i in input("Enter numbers separated by space:").split(" "):
-    a.append(int(i))
-print(a)
-swapped=0
+a=[int(i) for i in input("Enter numbers separated by space:").split(" ")]
 for i in range(len(a)-1):
     for j in range(len(a)-i-1):
         k = j + 1
         if a[j] > a[k]:
             a[j],a[k] = a[k],a[j]
-            swapped=1
-    if swapped == 0:
-        break
 print(a)
